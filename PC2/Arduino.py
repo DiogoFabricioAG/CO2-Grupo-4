e23ab0ed9bca37c9b7c43485bb141c6070c58968
Mati-az <matiaz.chevez.c@uni.pe>
2025-04-28 21:25:58 -0500

    arduino guardar datos

--- PC2/Arduino.py
+++ PC2/Arduino.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import csv
+from datetime import datetime
 
 puerto_arduino = 'COM5'
 velocidad_trans = 9600  
@@ -11,6 +13,13 @@ except Exception as e:
     print(f"Error al conectar con el puerto {puerto_arduino}: {e}")
     exit()
 
+nombre_archivo = 'datos_arduino.csv'
+
+with open(nombre_archivo, mode='w', newline='') as archivo:
+    escritor = csv.writer(archivo)
+    escritor.writerow(['Hora', 'LdrValorAnalog', 'LdrVoltaje', 'LdrResistencia', 'Temperatura', 'Humedad'])
+
+
 def leer_datos():
     if s.in_waiting > 0:
         data = s.readline().decode('utf-8').strip()
@@ -20,7 +29,7 @@ def leer_datos():
 def calcular_RLDR(Vout, Vin, Rfija):
     if Vin == Vout:
         raise ValueError("Vout no puede ser igual a Vin")
-    Rldr = (Vout * Rfija) / (Vin - Vout)
+    Rldr = Rfija*(Vin/Vout - 1)
     return Rldr
 
 while True:
@@ -30,18 +39,28 @@ while True:
 
             data = data.split(',')
             LdrValorAnalog = float(data[0]) # Valor analógico del LDR, entre 0 y 1023, lo cual representa de 0V y 5V
+            if(LdrValorAnalog == 0):
+                LdrValorAnalog = 0.1
             Temperatura = float(data[1])
             Humedad = float(data[2])
 
             # Convertir el valor analógico a voltaje (ValueAnalog/1023)*5.0
-            LdrValor = (LdrValorAnalog/1023.0)* 5.0
-            LdrValor = round(LdrValor, 2)
+            LdrValorVoltaje = (LdrValorAnalog/1023.0)* 5.0
+            LdrValorVoltaje = round(LdrValorVoltaje, 4)
 
-            # Calcular la resistencia del LDR
-            Rldr = calcular_RLDR(LdrValor, 5.0, 220)
-            Rldr = round(Rldr, 2)
+            LdrResistencia = calcular_RLDR(LdrValorVoltaje, 5.0, 220)
+            LdrResistencia = round(LdrResistencia, 2)
 
-            print(f"Valor analogico ldr: {LdrValorAnalog}, Voltaje ldr: {LdrValor}, Resistencia ldr: {Rldr} / Temperatura: {Temperatura}°C , Humedad: {Humedad}%")
+            hora_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            with open(nombre_archivo, mode='a', newline='') as archivo:
+                escritor = csv.writer(archivo)
+                escritor.writerow([hora_actual, LdrValorAnalog, LdrValorVoltaje, LdrResistencia, Temperatura, Humedad])
+
+
+
+            # El circuito divisor de voltaje se compone de una resistencia fija de 220 ohmios y el LDR, con un voltaje de entrada de 5V.
+            print(f"{hora_actual} -> Valor analogico ldr: {LdrValorAnalog}, Voltaje ldr: {LdrValorVoltaje}V, Resistencia ldr: {LdrResistencia}Ω / Temp: {Temperatura}°C, Humedad: {Humedad}%")
 
         time.sleep(1)
 
