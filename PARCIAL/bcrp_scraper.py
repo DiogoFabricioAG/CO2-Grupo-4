aa4b4a22a98441ca2dac8f58912514b5d9c4ee4b
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-05-13 20:08:36 -0500

    Scrap Parcial v3

--- PARCIAL/bcrp_scraper.py
+++ PARCIAL/bcrp_scraper.py
@@ -3,22 +3,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import csv
 import os
 
-
 def obtener_datos():
-    # Configurar Chrome con Webdriver Manager
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Modo sin interfaz gráfica
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    # Configurar Chrome para GitHub Actions
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     try:
         current_date = datetime.now()
         start_date = "2006-8"
